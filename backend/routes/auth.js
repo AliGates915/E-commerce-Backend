c9a921dbcea8d2ec89cf74d9ef31ebd32b31879a
Ali Gates <hacktech877@gmail.com>
2025-08-04 10:45:37 +0500

    Delete User

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { updateUserStatus } from "../controllers/authController.js";
+import { updateUserStatus, deleteUser } from "../controllers/authController.js";
 import { register, login, getAllUsers, forgotPassword, resetPassword } from "../controllers/authController.js";
 
 const router = express.Router();
@@ -13,4 +13,6 @@ router.post("/reset-password", resetPassword);
 router.get("/users", getAllUsers);
 
 router.patch("/users/:id/status", updateUserStatus);
+router.delete("/users/:id", deleteUser);
+
 export default router;
