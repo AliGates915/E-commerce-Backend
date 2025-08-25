56c7b1b62fdd3560c6e1b8f6f3aea3fb60c55df2
Ali Gates <hacktech877@gmail.com>
2025-08-25 16:39:49 +0500

    set yr code

--- backend/app.js
+++ backend/app.js
@@ -34,13 +34,11 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Permissions-Policy",
-    'geolocation=(self "https://wahidfoodssmc.com")'
-  );
-  next();
-});
+res.setHeader(
+  "Permissions-Policy",
+  'geolocation=(self "https://wahidfoodssmc.com" "https://geo.cardinalcommerce.com")'
+);
+
 
 
 // // Stripe webhook needs raw body
