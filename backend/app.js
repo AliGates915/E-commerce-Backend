29ecd3ba6b7770abbd9f0fe01752718e1eb41465
Ali Gates <128673394+AliGates915@users.noreply.github.com>
2025-12-16 15:20:18 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ const corsOptions = {
     'http://localhost:8081',
     "https://sky-link-e-commerce.vercel.app",
     "https://e-commerce-infinity-byte-10.vercel.app",
-    "",
+    "https://core-tech-ecommerce.vercel.app",
     "https://core-tech-ecommerce-beta.vercel.app",
     'https://e-commerce-infinity-byte.vercel.app',
     'http://localhost:5500'
